e3617b32ca55649e2bc2d859836a8dd92a151576
malaradobe <120513156+malaradobe@users.noreply.github.com>
2025-05-01 08:58:50 -0700

    Fixed chain command
    Fixed chain command

--- ui.tests/test-module/cypress/e2e/assets.cy.js
+++ ui.tests/test-module/cypress/e2e/assets.cy.js
@@ -48,7 +48,8 @@ describe('AEM Assets', () => {
         cy.get('dam-chunkfileupload.dam-ChunkFileUpload > input').first().selectFile(localPath, {force: true})
 
         // rename image
-        cy.get('input#dam-asset-upload-rename-input').clear().type(remoteImageName, {force: true});
+        cy.get('input#dam-asset-upload-rename-input').clear();
+        cy.get('input#dam-asset-upload-rename-input').type(remoteImageName, {force: true});
 
         // Press the upload button.
         cy.get('coral-dialog.is-open coral-dialog-footer [variant="primary"]').click({force: true});
